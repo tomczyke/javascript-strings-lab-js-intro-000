37e4d50313ebcfedd0096b3fb28e4642610f4982
Evan Tomczyk <tomczykej@gmail.com>
2019-07-06 19:21:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var specialGuest = "Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "space";
+var topic = "Let's talk about space";
 
+<<<<<<< HEAD
 var conversation = `Let's talk about ${topic}.`;
+=======
+var conversation = ${topic};
+>>>>>>> 289da5702eeb392fdbe05b4192b8feda145d30b3
